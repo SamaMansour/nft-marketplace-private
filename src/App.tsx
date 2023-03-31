1d2ec548a9af45c26e5b64019035226124351659
SamaMansour <sma302000@gmail.com>
2023-04-01 00:47:55 +0300

    Add switch button

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './app/components/Header/Header';
+import Footer from './app/components/Footer/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,23 +1,27 @@
 import react from "react";
 import { Link } from "react-router-dom";
 import '../../styles/Header.css'
+import Logo from "../../assets/images/Logo.png"
+import SwitchButton from '../../components/Switch/Switch';
 
 const Header = () => {
     return (
+      <div id="base">
         <div id="header">
-        <Link to='/' id='logo'>NFT Room</Link>
-
-        <div id="link-containers">
-          <a>Home</a>
-          <a>Marketplace</a>
-          <a>Community</a>
-          <a>Activity</a>
-          <a>Blog</a>
-
-
+        <Link to='/' id='logo'><img src={Logo} /></Link>
+        <div id="navigation">
+          <a className="link" href="#">Home</a>
+          <a className="link" href="#">Marketplace</a>
+          <a className="link" href="#">Community</a>
+          <a className="link" href="#">Activity</a>
+          <a className="link" href="#">Blog</a>
+        </div>
+        <div id="call-to-action">
+          <SwitchButton/>
           <button id="connect-wallet" >Connect Wallet</button>
         </div>
       </div>
+    </div>
     );
 }
 
