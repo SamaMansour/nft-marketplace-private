4fb05e368621c0d0a30b84dd3f1faff639c2ef68
SamaMansour <sma302000@gmail.com>
2023-04-09 16:25:46 +0300

    Add seller section

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import HeroSection from '../components/ui/HeroSection/HeroSection';
+import SellerSection from '../components/ui/SellerSection/SellerSection';
+import '../styles/home.scss'
 
 const Home = () => {
   return (
     <>
     <HeroSection />
+    <div className='seller-section'>
+    <SellerSection />
+    </div>
     </>
   )
 }
